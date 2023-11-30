825ebdf115c556328b197d48d9731057518675f7
João Pedro <joaopdesousa12@gmail.com>
2023-11-30 13:49:57 -0300

    User Resource Update: add findById, insert, put, delete

--- src/main/java/application/resources/UserResource.java
+++ src/main/java/application/resources/UserResource.java
@@ -1,4 +1,52 @@
 package application.resources;
 
+import application.entities.User;
+import application.services.UserService;
+import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Optional;
+
+@Resource
+@RestController
+@RequestMapping(value = "/u")
 public class UserResource {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Optional<User>> findById(@PathVariable Long id){
+        Optional<User> user = service.findById(id);
+        return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user){
+        user = service.insert(user);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequestUri()
+                .path("/{id}")
+                .buildAndExpand(user.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(user);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user){
+        user = service.update(id,user);
+
+        return ResponseEntity.ok().body(user);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
