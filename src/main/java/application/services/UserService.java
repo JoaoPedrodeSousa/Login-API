7312da25951e422195af01fc89622f3fa997118f
João Pedro <joaopdesousa12@gmail.com>
2023-11-30 23:18:29 -0300

    User log or not

--- src/main/java/application/services/UserService.java
+++ src/main/java/application/services/UserService.java
@@ -14,8 +14,17 @@ public class UserService {
     private UserRepository repository;
 
     public Optional<User> findById(Long id){
-        return repository.findById(id);
+        Optional<User> user = repository.findById(id);
+
+        if(user.get().getLogged() == Boolean.FALSE){
+            user.get().setLogged(Boolean.TRUE);
+            return user;
+        }
+
+        throw new RuntimeException("Usuário já está logado!");
     }
+
+
     public User insert(User user){
         return repository.save(user);
     }
@@ -23,14 +32,16 @@ public class UserService {
     public User update(Long id, User user){
         User entity = repository.getReferenceById(id);
         updateData(entity,user);
-        return repository.save(user);
+        return repository.save(entity);
     }
 
     private void updateData(User entity, User user){
         entity.setName(user.getName());
+        entity.setUsername(user.getUsername());
         entity.setEmail(user.getEmail());
         entity.setPhone(user.getPhone());
         entity.setPassword(user.getPassword());
+        entity.setLogged(user.getLogged());
     }
 
     public void delete(Long id){
